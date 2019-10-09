65280bdcab5167439e6699745703db5f23383161
Chris Young <beesucker@gmail.com>
2019-10-08 17:59:01 -0700

    for some reason I cant see args when spring is running :(

--- lib/generators/hecks/scaffold_generator.rb
+++ lib/generators/hecks/scaffold_generator.rb
@@ -1,16 +1,16 @@
 require "rails/generators/rails/scaffold_controller/scaffold_controller_generator.rb"
 
-module Hecks
+# path = File.expand_path('../templates', __dir__)  
+
+# HecksOnRails::Engine.config.app_generators do |g|
+#   g.templates << File.expand_path('../templates', __dir__)
+#   g.orm false
+# end
+
+class Hecks
   class ScaffoldGenerator < Rails::Generators::ScaffoldControllerGenerator
     attr_reader :aggregate_name, :domain_object_name
-    
-
-
     def create_controller_files
-
-      
-
-      puts HecksOnRails::Engine.config.app_generators.templates
       @aggregate_name = class_name.split('::').first
       @domain_object_name = class_name.split('::').last
       super
--- lib/hecks_on_rails/engine.rb
+++ lib/hecks_on_rails/engine.rb
@@ -3,9 +3,8 @@ require 'hecks-app'
 module HecksOnRails
   class Engine < ::Rails::Engine
     config.app_generators do |g|
-      if ARGV[0].try(:include?, 'hecks')
-        g.templates << File.expand_path('../templates', __dir__)
-      end
+      g.scaffold = false
+      g.templates << File.expand_path('../templates', __dir__)
       g.orm false
     end
   end
